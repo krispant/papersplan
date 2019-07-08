2b8d1611a5438c6dbd5eaba904f9194c931db416
Krishna <krishnachandrapant@gmail.com>
2019-07-08 23:46:44 +0530

    updated the app index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,9 @@
 import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
-
-import {Home, NotFound,TaxCalculation} from '../'
-import Navbar from 'react-bootstrap/Navbar'
-import logo from './logo.jpg'; 
-import Nav from 'react-bootstrap/Nav'
 
 import HeaderNavigation from '../HeaderNavigation'
 import Disclaimer from '../Disclaimer'
 import Routing from '../Routing'
 
-import NavDropdown from 'react-bootstrap/NavDropdown'
 
 class App extends Component {
   render() {
